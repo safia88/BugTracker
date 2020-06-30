77b1ccf6dd20ed9e8e4a03da236294897c75d020
Safia Ali <safialiusa@gmail.com>
2020-06-30 23:12:07 +0600

    user ticket page done

--- BugsApp/models.py
+++ BugsApp/models.py
@@ -6,24 +6,30 @@ from django.contrib.auth.models import AbstractUser
 
 
 class Customeuser(AbstractUser):
-    profile=models.CharField(null=True,max_length=100)
-    Age=models.IntegerField(null=True)
+    profile = models.CharField(null=True, max_length=100)
+    Age = models.IntegerField(null=True)
 
-User=settings.AUTH_USER_MODEL
+
+User = settings.AUTH_USER_MODEL
+
 
 class Ticket(models.Model):
     title = models.CharField(max_length=100)
     Description = models.TextField()
-    filed_by = models.CharField(max_length=100)
-    assigned_to = models.ForeignKey(User,related_name='assigned_to',on_delete=models.CASCADE,null=True)
-    completed_by = models.ForeignKey(User, related_name='completed_by',on_delete=models.CASCADE, null=True)
+    filed_by = models.ForeignKey(
+        User, related_name='filed_by', on_delete=models.CASCADE, null=False)
+    assigned_to = models.ForeignKey(
+        User, related_name='assigned_to', on_delete=models.CASCADE, null=True)
+    completed_by = models.ForeignKey(
+        User, related_name='completed_by', on_delete=models.CASCADE, null=True)
     LEVEL_CHOICES = (
         ('FIRST', 'New'),
         ('SECOND', 'In Progress'),
         ('THIRD', 'Done'),
         ('FOURTH', 'Invalid'),
     )
-    status = models.CharField(max_length=40, choices=LEVEL_CHOICES, default='FIRST')
+    status = models.CharField(
+        max_length=40, choices=LEVEL_CHOICES, default='FIRST')
     time_submited = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
--- BugsApp/views.py
+++ BugsApp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,reverse
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponseRedirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
@@ -6,77 +6,99 @@ from . import forms
 from . models import Ticket
 # Create your views here.
 
+
 @login_required
 def home(request):
-    ticket_new=Ticket.objects.filter(status='FIRST')
-    ticket_inprogress=Ticket.objects.filter(status='SECOND')
-    ticket_done=Ticket.objects.filter(status='THIRD')
-    context={'first':ticket_new,'second':ticket_inprogress,'third':ticket_done}
-    return render(request,'home.html',context)
+    ticket_new = Ticket.objects.filter(status='FIRST')
+    ticket_inprogress = Ticket.objects.filter(status='SECOND')
+    ticket_done = Ticket.objects.filter(status='THIRD')
+    ticket_discard = Ticket.objects.filter(status='FOURTH')
+    context = {'first': ticket_new,
+               'second': ticket_inprogress, 'third': ticket_done,
+               'fourth': ticket_discard}
+    return render(request, 'home.html', context)
+
+
+@login_required
+def user_view(request, id):
+    html = 'userdata.html'
+
+    created_ticket = Ticket.objects.filter(filed_by=id)
+    assigned_ticket = Ticket.objects.filter(assigned_to=id)
+    completed_ticket = Ticket.objects.filter(completed_by=id)
+
+    return render(request, html, {'created_ticket': created_ticket, 'assigned_ticket': assigned_ticket, 'completed_ticket': completed_ticket})
+
 
 @login_required
 def inserticket(request):
-    form=forms.Ticketform()
-    if request.method=='POST':
-        form=forms.Ticketform(request.POST)
+    form = forms.Ticketform()
+    if request.method == 'POST':
+        form = forms.Ticketform(request.POST)
         if form.is_valid():
-            post=form.save(commit=False)
-            post.filed_by=request.user
+            post = form.save(commit=False)
+            post.filed_by = request.user
             post.save()
             return redirect('home')
-    return render(request,'insert.html',{'form':form})
+    return render(request, 'insert.html', {'form': form})
+
 
 @login_required
-def ticket_edit(request,id):
-    ticket=Ticket.objects.get(id=id)
-    if request.method=='POST':
-        form=forms.EditForm(request.POST,instance=ticket)
+def ticket_edit(request, id):
+    ticket = Ticket.objects.get(id=id)
+    if request.method == 'POST':
+        form = forms.EditForm(request.POST, instance=ticket)
         if form.is_valid():
-            post=form.save(commit=False)
-            if post.assigned_to==None and post.completed_by==None and post.filed_by!=None:
-                post.status='FIRST'
+            post = form.save(commit=False)
+            if post.assigned_to == None and post.completed_by == None and post.filed_by != None:
+                post.status = 'FIRST'
                 post.save()
                 return redirect(home)
-            elif post.assigned_to!=None and post.completed_by==None:
-                post.status='SECOND'
+            elif post.assigned_to != None and post.completed_by == None:
+                post.status = 'SECOND'
                 post.save()
                 return redirect(home)
-            elif post.assigned_to!=None and post.completed_by!=None:
+            elif post.assigned_to != None and post.completed_by != None:
                 post.status = 'THIRD'
                 post.save()
                 return redirect(home)
             post.save()
             return redirect(home)
-    form=forms.EditForm(instance=ticket)
-    return render(request,'ticket_edit.html',{'form':form})
+    form = forms.EditForm(instance=ticket)
+    return render(request, 'ticket_edit.html', {'form': form})
+
 
 @login_required
-def set_invalid(request,id):
+def set_invalid(request, id):
     ticket = Ticket.objects.get(id=id)
-    ticket.status='FOURTH'
+    ticket.status = 'FOURTH'
     ticket.save()
     return redirect(home)
 
+
 @login_required
-def assigned_you(request,id):
+def assigned_you(request, id):
     ticket = Ticket.objects.get(id=id)
-    ticket.assigned_to=request.user
-    ticket.status='SECOND'
+    ticket.assigned_to = request.user
+    ticket.status = 'SECOND'
     ticket.save()
     return redirect(home)
 
+
 @login_required
-def completed_you(request,id):
+def completed_you(request, id):
     ticket = Ticket.objects.get(id=id)
-    ticket.completed_by=request.user
-    ticket.status='THIRD'
+    ticket.completed_by = request.user
+    ticket.status = 'THIRD'
     ticket.save()
     return redirect(home)
 
+
 @login_required
-def ticket_detail(request,id):
+def ticket_detail(request, id):
     ticket = Ticket.objects.get(id=id)
     return render(request, 'details.html', {'ticket': ticket})
+
 
 @login_required
 def signup(request):
@@ -96,25 +118,3 @@ def signup(request):
 def logout_action(request):
     logout(request)
     return redirect(request.GET.get("next", reverse('login')))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
